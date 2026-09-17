fa7cd5d27f4fdf231fc904b63fab6f5a0dba641d
CiszukoAntony <fplayersoffcial@gmail.com>
2026-09-17 03:11:38 -0400

    Add new icon image to the public images directory

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@ echo ==========================================
 echo Compilando nueva version de NameTitleApp...
 echo ==========================================
 
-pyinstaller --onefile --name nametitleapp src/main.py --clean
+pyinstaller --onedir --icon="public/images/icon.ico" --version-file="file_version_info.txt" --console --hidden-import=typer --hidden-import=rich --paths=src src/main.py --name nametitle --clean
 
 echo ==========================================
 echo ¡Compilacion finalizada! Revisa la carpeta dist/
